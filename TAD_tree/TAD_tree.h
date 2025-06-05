77b9809590421a7a09a9a7120cb711fe35e8d364
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-06-05 13:39:41 -0300

    update

--- TAD_tree/TAD_tree.h
+++ TAD_tree/TAD_tree.h
@@ -12,13 +12,14 @@ struct node
 };
 typedef struct node Node;
 
-Node * tree_create_empty();
-Node * tree_insert(void * info, Node * lst, Node *rst);
-int tree_empty(Node * root);
-void tree_free(Node * root);
-void tree_map(Node * root, void(operation)(void*));
-Node * tree_filter(Node *root, int condition(void*));
-Node * tree_search(Node * root, int(condition)(void*));
+int tree_empty(Node* root);
+Node* tree_create_empty();
+Node* tree_create_node(void* info, Node* lst, Node* rst);
+int tree_insert_node(Node* root, Node* new_node, int (compare)(void*, void*));
+void tree_free(Node* root);
+void tree_map(Node* root, void (operation)(void*));
+Node* tree_filter(Node* root, int (condition)(void*));
+Node* tree_search(Node* root, int (condition)(void*));Node * tree_create_empty();
 
 
     #include "TAD_tree.c"
